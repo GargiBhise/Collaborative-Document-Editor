ecb537906cc49d229449663d0b65cc72c2600de9
GargiBhise <gargib.2496@gmail.com>
2024-10-27 16:25:20 -0700

    Added logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,31 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>Home page</div>
+        <div className="homePageWrapper">
+            <div className="formWrapper">
+                <img src = "/logo.png" alt = "Collab Doc Logo"/>
+                <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+                <div className="inputGroup">
+                    <input type = "text" 
+                    className="inputBox" 
+                    placeholder="ROOM ID" />
+
+                    <input type = "text" 
+                    className="inputBox" 
+                    placeholder="USERNAME" />
+
+                    <button className="btn joinBtn">Join</button> 
+                    <span className="createInfo">
+                        If you don't have an invite then create &nbsp;
+                        <a href="" className="createNewBtn">
+                            new room</a>
+                    </span>
+                </div>
+            </div>
+            <footer>
+                <h4>Developed by <a href="https://github.com/GargiBhise/Collaborative-Document-Editor"> Gargi Bhise</a></h4>
+            </footer>
+        </div>
     )
 }
 
