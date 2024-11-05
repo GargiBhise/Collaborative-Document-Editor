8a3c6d74ce52dc743e669f0854f884e0c99b8e27
GargiBhise <gargib.2496@gmail.com>
2024-11-04 22:50:51 -0800

    text formatting toolbar (bold, colors, lists, etc.)

--- src/component/EditorPage.js
+++ src/component/EditorPage.js
@@ -3,6 +3,20 @@ import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import "./styles.css";
 
+const TOOLBAR_OPTIONS = [
+    [{header:[1,2,3,4,5,6, false]}],
+    [{font: []}],
+    [{list: "ordered"}, {list: "bullet"}],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{color: []}, {background: []}],
+    [{script: "sub"}, {script: "super"}],
+    [{align: []}],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+];
+
+
+
 export default function EditorPage() {
   // Create and manage editor instance. 
   //Creates a fresh editor container each time
@@ -16,7 +30,7 @@ export default function EditorPage() {
     const editor = document.createElement("div")
     wrapper.append(editor)
     // Initialize Quill
-    new Quill(editor, { theme: "snow" })
+    new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS} })
   }, []) // Empty deps array = run once
 
   return <div className="container" ref={wrapperRef}></div>
